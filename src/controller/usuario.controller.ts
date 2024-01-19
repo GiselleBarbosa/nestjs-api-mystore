59d2f105e356790accb545a7f6086a3e35222b6b
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-19 19:51:40 -0300

    criado um repository para salvar os usuarios criados

--- src/controller/usuario.controller.ts
+++ src/controller/usuario.controller.ts
@@ -1,14 +1,13 @@
+import { UsuarioRepository } from './../repository/usuario.repository';
 import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('/usuarios')
 export class UsuarioController {
+  private UsuarioRepository = new UsuarioRepository();
+
   @Post()
   async criarUsuario(@Body() dadosDoUsuario) {
+    this.UsuarioRepository.salvar(dadosDoUsuario);
     return dadosDoUsuario;
   }
 }
-
-/*  @Post()
-  async criarUsuario(@Body() dadosDoUsuario) {
-  return { message: 'Usuário criado com sucesso!', dadosDoUsuario }; 
-  } */
