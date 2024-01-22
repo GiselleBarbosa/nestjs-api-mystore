938e9d0196213fb52bd639ead457ecad03260b87
GiselleBarbosa <g88barbosa@gmail.com>
2024-01-22 09:58:27 -0300

    criado metodo de remover usuario

--- src/usuarios/controllers/usuario.controller.ts
+++ src/usuarios/controllers/usuario.controller.ts
@@ -3,7 +3,7 @@ import { CriaUsuariosDTO } from '../dto/CriaUsuario.dto';
 import { ListaUsuarioDTO } from '../dto/ListaUsuario.dto';
 import { UsuarioEntity } from '../entity/Usuario.entity';
 import { UsuarioRepository } from '../repository/usuario.repository';
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 
 @Controller('/usuarios')
@@ -36,11 +36,21 @@ export class UsuarioController {
 
   @Put('/:id')
   async atualizarUsuarios(@Param('id') id: string, @Body() novosDados: AtualizaUsuariosDTO) {
-  const usuarioAtualizado = await  this.usuarioRepository.atualizar(id, novosDados);
+    const usuarioAtualizado = await this.usuarioRepository.atualizar(id, novosDados);
 
-  return {
-    usuario: usuarioAtualizado, 
-    message: 'Usuário atualizado com sucesso.'
+    return {
+      usuario: usuarioAtualizado,
+      message: 'Usuário atualizado com sucesso.',
+    };
   }
+
+  @Delete('/:id')
+  async removerUsuario(@Param('id') id: string) {
+    const usuarioRemovido = await this.usuarioRepository.remover(id);
+
+    return {
+      usuario: usuarioRemovido,
+      message: 'Usuário removido com sucesso.',
+    };
   }
 }
